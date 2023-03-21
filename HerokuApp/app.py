c6e2e04c23ccb63f65c21db13dfe72d6e157b8bf
sujalaggarwal21105036 <sujalaggarwal8109@gmail.com>
2023-03-21 17:34:49 +0530

    Adding local files to git

---
+++ HerokuApp/app.py
@@ -0,0 +1,29 @@
+import openai
+import urllib.request
+from PIL import Image
+import streamlit as st
+
+openai.api_key = "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"
+
+#Function to generate image
+def generate_img(img_description):
+    img_response=openai.Image.create(
+        prompt=img_description,
+        n=1,
+        size="512x512"
+    )
+    img_url=img_response['data'][0]['url']
+    urllib.request.urlretrieve(img_url,'img.png')
+    img=Image.open("img.png")
+    return img
+
+# Page Title
+st.title(' Image Generation ')
+
+#Text box
+img_description=st.text_input('Image Description')
+
+#Text button
+if st.button('Generate Image'):
+    generated_image=generate_img(img_description)
+    st.image(generated_image)
